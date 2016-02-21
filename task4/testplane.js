83f7112a99b53621083e0971c3679aecf6f4260d
Dmitriy Tishchenko <dtishchenko@spb.dataartuk.com>
2016-02-22 02:02:38 +0300

    Tasks 3 & 4

---
+++ task4/testplane.js
@@ -0,0 +1,29 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3,
+        "sourceFile"    : "testplane.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 3623,
+        "faces"         : 3725,
+        "normals"       : 3563,
+        "uvs"           : 0,
+        "materials"     : 1
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.8, 0.8],
+	"colorSpecular" : [0.8, 0.8, 0.8],
+	"illumination" : 2,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0
+	}],
+
+    "buffers": "testplane.bin"
+
+}
